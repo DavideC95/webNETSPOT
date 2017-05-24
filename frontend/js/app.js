8641984c5eb077e977d5ad89fe7d0bdf6bb4f036
Helias <stefanoborzi32@gmail.com>
2017-05-24 18:42:48 +0200

    Re-organized frontend directory structure, close #5

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,59 +1,9 @@
-﻿(function () {
-    'use strict';
+var app = angular.module('app', [ 'ui.router', 'ui.bootstrap' ]);
 
-    angular
-        .module('app', ['ngRoute', 'ngCookies'])
-        .config(config)
-        .run(run);
+app.controller('homeController', function ($scope) {
+  $scope.helloworld = "Hello world by AngularJS";
+});
 
-    config.$inject = ['$routeProvider', '$locationProvider'];
-    function config($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                controller: 'HomeController',
-                templateUrl: 'partials/home/home.html',
-                controllerAs: 'vm'
-            })
-             .when('/welcome', {
-                controller: 'welcomeController',
-                templateUrl: 'partials/welcome/welcome.html',
-                controllerAs: 'vm'
-            })
-
-            .when('/login', {
-                controller: 'LoginController',
-                templateUrl: 'partials/login/login.view.html',
-                controllerAs: 'vm'
-            })
-
-            .when('/register', {
-                controller: 'RegisterController',
-                templateUrl: 'partials/register/register.view.html',
-                controllerAs: 'vm'
-            })
-
-            .otherwise({ redirectTo: '/' });
-    }
-
-    run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
-    function run($rootScope, $location, $cookies, $http) {
-        // Mantiene l'utente loggato anche dopo il refresh
-        $rootScope.globals = $cookies.getObject('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
-        }
-       
-       /* 
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-            // Se si tenta di accedere a pagine senza permessi rimanda l'utente ala login page.
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register', '/']) === -1;
-            var loggedIn = $rootScope.globals.currentUser;
-            if (restrictedPage && !loggedIn) {
-                $location.path('/login');
-            }
-        });
-        */
-    }
-
-
-})();
+app.controller('pageController', function ($scope) {
+  $scope.example = "page";
+});
